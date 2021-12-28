7737c45102b174b6a8e6066a7d463cbc06f25467
Habib <s1169265@stud.uni-frankfurt.de>
2021-12-28 16:10:00 +0100

    Mobile receives message with new function //credits to bilbobx182

--- mobile/src/main/java/com/example/phonewearai/MainActivity.java
+++ mobile/src/main/java/com/example/phonewearai/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.phonewearai;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.wearable.MessageClient;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MessageClient.OnMessageReceivedListener {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //mTextView = findViewById(R.id.);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Wearable.getMessageClient(this).addListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Wearable.getMessageClient(this).removeListener(this);
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        String msg = new String(messageEvent.getData());
+        Log.i("Message received", msg);
+
     }
 }
--- wear/src/main/java/com/example/phonewearai/MainActivity.java
+++ wear/src/main/java/com/example/phonewearai/MainActivity.java
@@ -5,11 +5,21 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.example.phonewearai.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.wearable.CapabilityClient;
+import com.google.android.gms.wearable.CapabilityInfo;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
@@ -19,6 +29,13 @@ public class MainActivity extends Activity implements SensorEventListener {
     private SensorManager mSensorManager;
     private Sensor mHeart;
     private Sensor mTemperature;
+
+    private String transcriptionNodeId;
+
+    //credits https://github.com/Bilbobx182
+    private static final String SET_MESSAGE_CAPABILITY = "setString";
+    public static final String SET_MESSAGE_PATH = "/setString";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,22 +70,29 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(this, mHeart, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mTemperature, SensorManager.SENSOR_DELAY_NORMAL);
+
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent event){
         if (event.sensor.getType() == Sensor.TYPE_HEART_RATE){
             float heartRate = event.values[0];
-            heartView.setText("Heartrate: " + heartRate);
-            Log.i("HeartRate", "Puls hat sich geaendert: "+Float.toString(heartRate));
+            String strHeartRate = Float.toString(heartRate);
+            beginSendMessageToPhone(strHeartRate);
+            heartView.setText("Heartrate: " + strHeartRate);
+            Log.i("HeartRate", "Puls hat sich geaendert: "+ strHeartRate);
+
         } else if (event.sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE){
             float temp = event.values[0];
-            tempView.setText("Temperature: "+ temp);
-            Log.i("Temperature", "Temperatur hat sich geaendert: "+Float.toString(temp));
+            String strTemp = Float.toString(temp);
+
+            tempView.setText("Temperature: "+ strTemp);
+            //Log.i("Temperature", "Temperatur hat sich geaendert: "+ strTemp);
         }
     }
 
@@ -76,5 +100,62 @@ public class MainActivity extends Activity implements SensorEventListener {
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
-}
 
+    //credits https://github.com/Bilbobx182
+    private void beginSendMessageToPhone(String msg) {
+
+        AsyncTask.execute(() -> {
+
+            CapabilityInfo capabilityInfo;
+            try {
+
+                capabilityInfo = Tasks.await(
+                        Wearable.getCapabilityClient(getBaseContext()).getCapability(SET_MESSAGE_CAPABILITY, CapabilityClient.FILTER_REACHABLE));
+                updateTranscriptionCapability(capabilityInfo);
+                requestTranscription(msg.getBytes());
+
+                // END REFERENCE
+
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+    private void updateTranscriptionCapability(CapabilityInfo capabilityInfo) {
+        Set<Node> connectedNodes = capabilityInfo.getNodes();
+
+        transcriptionNodeId = pickBestNodeId(connectedNodes);
+    }
+
+
+    private String pickBestNodeId(Set<Node> nodes) {
+        String bestNodeId = null;
+        // Find a nearby node or pick one arbitrarily
+        for (Node node : nodes) {
+            if (node.isNearby()) {
+                return node.getId();
+            }
+            bestNodeId = node.getId();
+        }
+        return bestNodeId;
+    }
+
+
+
+    //credits https://github.com/Bilbobx182
+    private void requestTranscription(final byte[] message) {
+
+        AsyncTask.execute(() -> {
+            if (transcriptionNodeId != null) {
+                final Task<Integer> sendTask = Wearable.getMessageClient(getBaseContext()).sendMessage(transcriptionNodeId, SET_MESSAGE_PATH, message);
+
+                sendTask.addOnSuccessListener(dataItem -> Log.d("MESSAGESTATE", "SUCCESS"));
+                sendTask.addOnFailureListener(dataItem -> Log.d("MESSAGESTATE", "FAILURE"));
+                sendTask.addOnCompleteListener(task -> Log.d("MESSAGESTATE", "COMPLETE"));
+            }
+        });
+    }
+
+}
